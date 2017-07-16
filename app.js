d76a3100545b9f967c5cddf6b275922955f428a6
kies <kiesmohamed@outlook.com>
2017-07-16 14:42:42 +0200

    commit master+2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// commit de la branche djazair/myproject-alphorm
+// commit de la branche djazair/myproject-alphorm+2
 app.use('/', index);
 app.use('/users', users);
 

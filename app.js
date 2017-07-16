f25a02e9379aec498646c813ee20ce01f14077d6
kies <kiesmohamed@outlook.com>
2017-07-16 11:15:08 +0200

    test mokies

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Found');
+  var err = new Error('Found kies');
   err.status = 404;
   next(err);
 });
